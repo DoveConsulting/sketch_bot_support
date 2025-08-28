472e756575c61758474ccbcb226bf6367d2a095f
dove <demola.oris@gmail.com>
2025-08-27 21:37:59 -0500

    Initial commit

---
+++ launch/view_robot.launch.py
@@ -0,0 +1,45 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import os
+from ament_index_python.packages import get_package_share_directory
+import xacro
+
+def generate_launch_description():
+    urdf = xacro.process_file(os.path.join(get_package_share_directory('sketch_bot_support'), "urdf/sketch_bot.xacro"))
+    robot_description_content = urdf.toprettyxml(indent='  ')
+    robot_description = {"robot_description": robot_description_content}
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("sketch_bot_support"), "rviz", "view_robot.rviz"]
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    nodes_to_start = [
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+        rviz_node,
+    ]
+
+    return LaunchDescription(nodes_to_start)
